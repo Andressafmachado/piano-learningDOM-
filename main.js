c6fb0cacde2f1b2ad014255b281755af7cfced1c
andressafmachado <andressamachado98@gmail.com>
2021-02-13 15:13:23 +0100

    piano working

--- main.js
+++ main.js
@@ -3,60 +3,58 @@
 const keys = document.querySelectorAll(".key")
 
 function playNote(event) {
+   let audioKeyCode = getKeyCode(event);
 
+   const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+   
+
+    const cantFoundAnyKey = !key
+    if(cantFoundAnyKey) {
+        return;
+    }
+   addPlayingClass(key)
+   playAudio(audioKeyCode)
 
 }
+   
+function addPlayingClass(key){
+    key.classList.add("playing")
+}
 
-let keyCode;
+function getKeyCode(event){
+    let keyCode;
 
 const isKeyboard = event.type === "keydown"
     if(isKeyboard){
-      keyCode = event.keyCode  
+      keyCode = event.keyCode 
     } else{
         keyCode = event.target.dataset.key
     }
-    return keyCode;
+    return keyCode
 }
 
+function playAudio(audioKeyCode) {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime= 0;
+    audio.play()
+}
 
+function removePlayingClass(event){
+    event.target.classList.remove("playing")
+}
 
 keys.forEach( function(key){
     key.addEventListener("click", playNote)
+    key.addEventListener("transitionend", removePlayingClass)
    })
 
 window.addEventListener("keydown", playNote)
 
 
-// const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
 
 
 
-// 
-
-// 
-//      let audioKeyCode = getKeyCode(event); 
-//     }
 
 
 
-// const cantFoundAnyKey = !key
-//     if(cantFoundAnyKey){
-//     return;
-//     }
-
-// playAudio(audioKeyCode)
-
-
-
-// function getKeyCode(event) {
-//     let keyCode;
-
-
-
-// //play audio
-// function playAudio(audioKeyCode){
-// const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
-// audio.currentTime = 0;
-// audio.play()
-// }
 
